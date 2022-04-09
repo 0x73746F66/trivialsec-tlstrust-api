2ca4467c2c9e68bf3494efe4a3689dfc69048229
chrisdlangton <chris@langton.cloud>
2022-04-10 00:39:44 +1000

    multiple root certs v0.0.2

--- src/output.py
+++ src/output.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from OpenSSL.crypto import X509
+from cryptography import x509
 from tlstrust import TrustStore
 from tlstrust.context import SOURCES, PLATFORMS, BROWSERS, LANGUAGES
 
 contexts = {**SOURCES, **PLATFORMS, **BROWSERS, **LANGUAGES}
 
-def to_json(trust_store: TrustStore, tlstrust_query: dict, **kwargs) -> dict:
+def to_json(trust_store: TrustStore, **kwargs) -> dict:
     results = []
     for name, is_trusted in trust_store.all_results.items():
         ctx = None
@@ -17,21 +18,19 @@ def to_json(trust_store: TrustStore, tlstrust_query: dict, **kwargs) -> dict:
         result['name'] = name
         result['is_trusted'] = is_trusted
         try:
-            result['exists'] = isinstance(trust_store.get_certificate_from_store(ctx), X509)
+            result['exists'] = isinstance(trust_store.certificate, X509)
             result['expired'] = trust_store.expired_in_store(ctx)
         except FileExistsError:
             result['exists'] = False
         results.append(result)
+    subject_common_name = trust_store.certificate.to_cryptography().subject.get_attributes_for_oid(x509.OID_COMMON_NAME)[0]._value
     return {
         '_metadata': {
             "fetch_time": datetime.utcnow().replace(microsecond=0).isoformat(),
-            "certificate_not_valid_after": tlstrust_query.get('not_valid_after'),
-            "certificate_issuer": tlstrust_query.get('certificate_issuer'),
-            "certificate_issuer_ski": tlstrust_query.get('issuer_ski'),
+            "certificate_not_valid_after": trust_store.certificate.to_cryptography().not_valid_after,
+            "certificate_issuer": subject_common_name,
+            "certificate_issuer_ski": trust_store.key_identifier,
         },
-        '_query': {
-            **kwargs,
-            "peer_address": tlstrust_query.get('peer_address'),
-        },
+        '_query': {**kwargs},
         'trust_stores': results
     }
