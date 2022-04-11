4775fc6798f618eaef2efb490de7df5a2b0c1d7e
chrisdlangton <chris@langton.cloud>
2022-04-12 01:25:22 +1000

    bump tlstrust

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'fastapi==0.75.1',
-        'tlstrust==2.4.0',
+        'tlstrust==2.4.1',
         'validators==0.18.2',
         'uvicorn[standard]'
     ],
